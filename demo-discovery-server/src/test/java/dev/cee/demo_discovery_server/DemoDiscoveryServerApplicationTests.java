c1e00a680f334db15b3956d56bd7b118fc51ff5e
MusiliC <musilibrian07@gmail.com>
2024-06-09 13:59:12 +0300

    Eureka discovery server and client for service tracking

---
+++ demo-discovery-server/src/test/java/dev/cee/demo_discovery_server/DemoDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cee.demo_discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
